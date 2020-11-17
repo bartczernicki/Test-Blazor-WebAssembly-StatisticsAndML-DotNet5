ea177b1226179674dc26fd3527ec5df15dcb8ec5
Bart Czernicki <bartczernicki@outlook.com>
2020-11-17 12:04:37 -0500

    Temp changes for performance test.

---
+++ Classes/PerformanceTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+
+namespace Test_Blazor_WebAssembly_StatisticsAndML_DotNet5
+{
+    public class PerformanceTest
+    {
+        public int ID { get; set; }
+
+        public string DistributionName { get; set; }
+
+        public int TrialsNumber { get; set; }
+
+        public int SamplesNumber { get; set; }
+
+        public double TestDuration { get; set; }
+
+        public double TakeSamples()
+        {
+            var dateTime = DateTime.Now;
+            var binomaial = new MathNet.Numerics.Distributions.Binomial(0.5, this.TrialsNumber);
+            var generatedsamples = binomaial.Samples().Take(SamplesNumber).ToArray();
+            var dateTimeElapsed = (DateTime.Now - dateTime).TotalMilliseconds;
+
+            return dateTimeElapsed;
+        }
+
+        public string GetTestName()
+        {
+            var fullName = $"{DistributionName}-Trials:{TrialsNumber}-Samples:{SamplesNumber}";
+            return fullName;
+        }
+    }
+}
