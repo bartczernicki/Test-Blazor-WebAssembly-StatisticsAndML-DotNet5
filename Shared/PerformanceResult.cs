9b178d1667bdaca7953bb1444cd85f1da6364765
Bart Czernicki <bartczernicki@outlook.com>
2020-11-16 22:25:45 -0500

    Temp changes for performance test.

---
+++ Shared/PerformanceResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_Blazor_WebAssembly_StatisticsAndML_DotNet5
+{
+    public class PerformanceResult
+    {
+        public int ID { get; set; }
+
+        public string TestName { get; set; }
+
+        public double Time { get; set; }
+    }
+}
